f8b1da4ae399707ebcdfc74ec88cf22fc0051da1
Wailt <vitaliy.miroshnychenko@magemojo.com>
2017-04-27 13:02:58 +0300

    Add new stats and skills

--- Stats.py
+++ Stats.py
@@ -8,16 +8,19 @@ class Stats(sprite.Sprite):
         sprite.Sprite.__init__(self)
 
         self.pos = Text(0, 0, '1')
-        self.skills = Text(0, 15, '2')
-        self.speed = Text(0, 30, '3')
+        self.athletics = Text(0, 15, '2')
+        self.fight = Text(0, 30, '3')
+        self.speed = Text(0, 45, '4')
         self.update(sp)
 
     def update(self, sp):
         self.pos.info = 'x:' + str(int(sp.x)) + ', y:' + str(int(sp.y))
-        self.skills.info = 'athletics:' + str(round(sp.skills['athletics'], 1))
+        self.athletics.info = 'athletics:' + str(round(sp.skills['athletics'], 1))
+        self.fight.info = 'fight:' + str(round(sp.skills['fight'], 1))
         self.speed.info = 'speed: ' + str(round(sp.speed, 3))
 
     def draw(self, screen):
         self.pos.draw(screen)
-        self.skills.draw(screen)
+        self.athletics.draw(screen)
+        self.fight.draw(screen)
         self.speed.draw(screen)
